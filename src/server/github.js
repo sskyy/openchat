c57aca479ce076030230b8f0af2d51487c9cc360
sskyy <skyking_H@hotmail.com>
2013-01-05 16:14:39 +0800

    t

--- src/server/github.js
+++ src/server/github.js
@@ -5,14 +5,27 @@ var spawn = require('child_process').spawn;
 exports.listen = function( server ){
     server.handlers.POST.registerPath.call(server.handlers.POST,"/github/update", function( req, res ){
         console.log( "github update call.");
-        var cmd = spawn('sudo', ['git','pull'], {'cwd':'/opt/openchat'});
+        var cmd = spawn('sudo', ['git','pull'], {
+            'cwd':'/opt/openchat'
+        });
         
         cmd.stdout.on("data",function(data){
+            console.log( "data:", data)
             res.writeHead(200, {
                 'Content-Type': 'text/html'
             });
             res.write(JSON.stringify({
                 message:'git pull success'
+            }));
+            res.end()
+        });
+        cmd.stderr.on("data",function(err){
+            console.log( "err:", err); 
+            res.writeHead(500, {
+                'Content-Type': 'text/html'
+            });
+            res.write(JSON.stringify({
+                message:'git pull error'
             }));
             res.end()
         });
@@ -39,7 +52,9 @@ exports.listen = function( server ){
     })
     server.handlers.GET.registerPath.call(server.handlers.GET,"/github/update", function( req, res ){
         console.log( "github update call.");
-        var cmd = spawn('sudo', ['git','pull'], {'cwd':'/opt/openchat'});
+        var cmd = spawn('sudo', ['git','pull'], {
+            'cwd':'/opt/openchat'
+        });
         
         cmd.stdout.on("data",function(data){
             res.writeHead(200, {
