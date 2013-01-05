eaa21bcb4516a7ed6081be64e4bca8161cfbc639
sskyy <skyking_H@hotmail.com>
2013-01-05 16:05:24 +0800

    t

--- src/server/github.js
+++ src/server/github.js
@@ -3,11 +3,38 @@
  */
 var spawn = require('child_process').spawn;
 exports.listen = function( server ){
-    server.handlers.POST.registerPath.call(server.handlers.POST,"./github/update", function( req, res ){
+    server.handlers.POST.registerPath.call(server.handlers.POST,"/github/update", function( req, res ){
         console.log( "github update call.");
         var cmd = spawn('cmd', ['/opt/openchat','sudo git pull']);
-        cmd.stderr.on("data", function( data  ){
-            console.log( "github pull err", data );
+        
+        cmd.stdout.on("data",function(data){
+            res.writeHead(200, {
+                'Content-Type': 'text/html'
+            });
+            res.write(JSON.stringify({
+                message:'git pull success'
+            }));
+            res.end()
+        });
+    
+        cmd.on("exit",function( code ){
+            if( code ==0 ){
+                res.writeHead(200, {
+                    'Content-Type': 'text/html'
+                });
+                res.write(JSON.stringify({
+                    message:'git pull success'
+                }));
+                res.end()
+            }else{
+                res.writeHead(500, {
+                    'Content-Type': 'text/html'
+                });
+                res.write(JSON.stringify({
+                    message:'git pull error'
+                }));
+                res.end()
+            }
         })
     })
     server.handlers.GET.registerPath.call(server.handlers.GET,"/github/update", function( req, res ){
